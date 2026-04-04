7dd84cc74b3a64c122f50186738a9cd278b275f1
Kiran <kirand09876@gmail.com>
2026-04-04 12:20:17 +0530

    Gunicorn

--- paymentsection/settings.py
+++ paymentsection/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # ───────────────────────────────────────────────
 # BASE DIR
@@ -116,10 +117,9 @@ WSGI_APPLICATION = 'paymentsection.wsgi.application'
 # DATABASE
 # ───────────────────────────────────────────────
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # ───────────────────────────────────────────────
